29c7e3d7eb878ba094ce5d430304510400298375
Cedric Travanca <cedric.travanca@gmail.com>
2024-06-05 10:47:43 +0200

    image et logo ct

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,9 @@
 // Enter all your detials in this file
 // Logo images
-import logogradient from "./assets/logo.svg";
-import logo from "./assets/logo2.svg";
+import logogradient from "./assets/LogoCT.png";
+import logo from "./assets/LogoCT.png";
 // Profile Image
-import profile from "./assets/profile.jpg";
+import profile from "./assets/profile.png";
 // Tech stack images
 import html from "./assets/techstack/html.png";
 import css from "./assets/techstack/css.png";
