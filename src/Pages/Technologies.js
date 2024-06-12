0c0d54261907b670b889529b71806c2071bc0637
Cedric Travanca <cedric.travanca@gmail.com>
2024-06-12 17:56:16 +0200

    compétences techniques

--- src/Pages/Technologies.js
+++ src/Pages/Technologies.js
@@ -25,7 +25,16 @@ function Technologies() {
           Compétences techniques
         </h1>
         <p className="text-content py-2 lg:max-w-3xl">
-          Technologies que j'ai utilisé
+          <p>Lors de ma formation, j'ai acquis certaines compérences en front-end, tel que: <br/><br/>
+            -Créer un système d'authentification<br/>
+            -Echanger avec une API<br/>
+            -Optimiser le SEO d'un site web<br/>
+            -Structurer mon code<br/>
+            -Planifier le developpement d'un site web<br/>
+            -Rendre une application dynamique et créer des routes<br/>
+            <br/>
+
+            Technologies que j'ai utilisé </p>
         </p>
       </section>
       <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
